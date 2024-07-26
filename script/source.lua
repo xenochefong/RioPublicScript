16687fbe88afa263aece60bc25fab3e7d51c3ff4
FluxTrux <mathewjaikirk2011@proton.me>
2024-07-26 20:44:43 +0100

    Create source.lua

---
+++ script/source.lua
@@ -0,0 +1,31 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Rio Universal V1.0.0",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "RioUniversal",
+      FileName = "RIOSCRIPTCONFIG"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+   KeySystem = false,
+   KeySettings = {
+      Title = " ",
+      Subtitle = " ",
+      Note = " ",
+      FileName = "RIOSCRIPTAUTH",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {" "}
+   }
+})
+
+local Main = Window:CreateTab("Universal", 4483362458)
+
+-- SOON
